444944975ac3a16c5b31a134fbdc6306ecff43f7
Rafia Sultana <rafiasultana0097@gmail.com>
2022-05-28 23:15:29 +0600

    cors updated

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afe3u.mongodb.net/?retryWrites=true&w=majority`;
